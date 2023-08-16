293876604c75983dd1c9c5f23be855db47625551
Projjwal <85036390+projyal007@users.noreply.github.com>
2023-08-16 23:27:24 +0530

    Fixing minor code issues

--- TicketBookingApp/src/main/java/com/ramsha/TicketBookingApp/service/AppService.java
+++ TicketBookingApp/src/main/java/com/ramsha/TicketBookingApp/service/AppService.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,31 +13,17 @@ import com.ramsha.TicketBookingApp.dao.AppDao;
 
 @Service
 public class AppService {
-	static final String url = "jdbc:mysql://127.0.0.1:3306/ttbs";
-    static final String user = "root";
-    static final String password = "22Ekish@22";
 	@Autowired
 	AppDao appDao;
+	@Autowired
+	DataSource dataSource;
 	public String signup(String fname, String lname, String phone, String username, String password2) throws SQLException
 	{
 		
-		Connection connection = null ;
-		connection = DriverManager.getConnection(url,user,password);
-		int loginId = appDao.createLoginDetails(connection,fname, lname, phone,username,password2);
-		appDao.createUserDetails(connection,loginId, fname, lname, phone, username, password2);
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            System.out.println( "Connection successful to database! " );
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            if (connection != null && !connection.isClosed())
-            {
-                connection.close();
-            }
-        }
+		try(Connection connection = dataSource.getConnection()){
+			int loginId = appDao.createLoginDetails(connection,fname, lname, phone,username,password2);
+			appDao.createUserDetails(connection,loginId, fname, lname, phone, username, password2);
+		}
 		return fname + lname + phone;
 	}
 }
